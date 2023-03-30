103ac0075710765cbf91b4e11a1d4bc318af1477
Samuelimmerso <samuelimmerso.info@gmail.com>
2023-03-30 02:01:13 +0200

    init

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('homepage');
+})->name('homepage');
+
+Route::get('/', function () {
+    return view('homepage');
+})->name('homepage');
+
+Route::get('/', function () {
+    return view('homepage');
+})->name('homepage');
